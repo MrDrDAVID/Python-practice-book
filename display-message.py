daf6b1c525d1108e36fcb8b422f09d92ec517853
David Garza <garzadavid26@yahoo.com>
2021-01-12 20:01:50 -0800

    Add files via upload

---
+++ display-message.py
@@ -0,0 +1,4 @@
+def display_message():
+    print('This chapter is about learing how to create functions.')
+
+display_message()
---
+++ favorite-book.py
@@ -0,0 +1,4 @@
+def fav_book(title):
+    print('One of my favorite books is ' + title)
+
+fav_book('The Bible')
