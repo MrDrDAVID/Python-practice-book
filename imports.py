3d9f38dcb693d018d591d20083ecf67c3b10414b
David Garza <garzadavid26@yahoo.com>
2021-01-25 15:41:57 -0800

    python crash course exercise

---
+++ imports.py
@@ -0,0 +1,7 @@
+import sandwiches
+from tshirt import make_shirt
+from magicians import show_magicians as sm
+
+sandwiches.make_sammiches('lettuce', 'cheese', 'tomatoes', 'chicken')
+make_shirt('Large', 'I love my boo so much')
+sm(['David Blaine', 'houdini', 'genie', 'magic man'])
